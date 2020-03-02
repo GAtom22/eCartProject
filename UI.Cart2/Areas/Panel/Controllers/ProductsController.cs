c0f060e9684f18f3e3856dbb74cd6900bfb857af
Tom <tomasguerraalda@hotmail.com>
2020-03-02 18:32:09 -0300

    Agregar carpeta Areas

---
+++ UI.Cart2/Areas/Panel/Controllers/ProductsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BusinessLogic.Cart;
+
+namespace UI.Cart.Areas.Panel.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Panel/Products
+        public ActionResult Index()
+        {
+            ProductsBL products = new ProductsBL();
+            return View(products.GetAll().ToList());
+        }
+    }
+}
---
+++ UI.Cart2/PanelAreaRegistration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI.Cart.Areas.Panel
+{
+    public class PanelAreaRegistration:AreaRegistration
+    {
+
+        public override string AreaName => "Panel";
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Panel_default",
+                "Panel/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+                );
+        }
+    }
+}
